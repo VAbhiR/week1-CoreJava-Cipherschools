72d7a2039ae2fb361b3dd499f3817f87384dc691
Abhinav <vundyalaabhinav@gmail.com>
2024-01-29 12:49:14 +0530

    Lecture4

---
+++ lecture4.java
@@ -0,0 +1,19 @@
+public class Lecture4 {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 7;
+    System.out.println("x is :" + x);
+    System.out.println("Y is " + y);
+    
+    byte b =10;
+    System.out.println("B is " + b);
+
+
+    byte b2 =(byte)129;
+    System.out.println("B is "+ b2);
+
+    int i = b;
+    System.out.println("I is " + i);
+
+  }
+}
